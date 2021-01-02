65105ffdd95b9ac0c2f45bb3f3d57b9d2ed68d92
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-01-01 23:51:48 -0300

    Variável referenciando método

--- Lambda04/Program.cs
+++ Lambda04/Program.cs
@@ -14,7 +14,9 @@ namespace Lambda04 {
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
 
-            List<string> result = list.Select(NameUpper).ToList();
+            Func<Product, string> func = NameUpper;
+
+            List<string> result = list.Select(func).ToList();
             foreach (string item in result) {
                 Console.WriteLine(item);
             }
