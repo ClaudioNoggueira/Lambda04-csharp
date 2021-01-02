63361c52a8623ec28eb744231898662f1037dad8
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-01-01 23:53:16 -0300

    Variável com expressão lambda

--- Lambda04/Program.cs
+++ Lambda04/Program.cs
@@ -14,16 +14,12 @@ namespace Lambda04 {
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
 
-            Func<Product, string> func = NameUpper;
+            Func<Product, string> func = p => p.Name.ToUpper();
 
             List<string> result = list.Select(func).ToList();
             foreach (string item in result) {
                 Console.WriteLine(item);
             }
-        }
-
-        static string NameUpper(Product p) {
-            return p.Name.ToUpper();
         }
     }
 }
