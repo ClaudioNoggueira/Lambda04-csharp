2ae3211bb59d66e27db123d02633cdb0e77849e8
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-01-01 23:48:52 -0300

    Referenciando método

--- Lambda04/Program.cs
+++ Lambda04/Program.cs
@@ -1,9 +1,27 @@
-﻿using System;
+﻿using Lambda04.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Lambda04 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            List<string> result = list.Select(NameUpper).ToList();
+            foreach (string item in result) {
+                Console.WriteLine(item);
+            }
+        }
+
+        static string NameUpper(Product p) {
+            return p.Name.ToUpper();
         }
     }
 }
